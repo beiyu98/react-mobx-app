6b9d3002ca3291ba1bb1bed2384eab6db8943a81
Bruce Zhang <bruceworkzone@outlook.com>
2018-04-09 23:45:50 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <h1>hello world</h1>
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src'),
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.jsx?/,
+                include:[path.resolve(__dirname,'src')],
+                exclude:[path.resolve(__dirname,'node_modules')],
+                use:'babel-loader',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'web-app',
+            meta: {
+                viewport: 'width=device-width,initial-scale=1',
+            },
+            inject:true,
+            hash:true,
+        })
+    ],
+    devServer:{
+        host:'127.0.0.1',
+        port:8090,
+        open:true,
+    } 
+}
